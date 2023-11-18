f3f01c37753cedd73b1a0f00f7feb7c9f003b929
Zhoujp <1228423514@qq.com>
2023-11-18 22:13:31 +0800

    添加 前缀、后缀数组问题（两道题，包括优化空间的版本） 判断两个字符串是否互为旋转词 咖啡杯问题（贪心+dp）

--- intermediate course/class4.cpp
+++ intermediate course/class4.cpp
@@ -9,7 +9,10 @@
  * 1.最小栈设计
  * 2.栈和队列的互相实现
  * 3.动态规划的空间压缩技巧
- * 最近修改日期：2023-11-17
+ * 4.前缀、后缀数组问题（两道题，包括优化空间的版本）
+ * 5.判断两个字符串是否互为旋转词（我这里用滚动哈希进行字符串匹配）
+ * 6.咖啡杯问题
+ * 最近修改日期：2023-11-18
  *
  * @author   Zhou Junping
  * @email    zhoujunpingnn@gmail.com
@@ -22,6 +25,8 @@
 #include <queue>
 #include <algorithm>
 #include <numeric>
+#include <cmath>
+#include <functional>
 
 using namespace std;
 
@@ -204,10 +209,212 @@ int Optimize_dp(vector<vector<int>>& matrix) {
 }
 
 
+/**
+ * 前缀数组、后缀数组的问题
+ * 且根据题目的要求，将空间复杂度优化到O(1)
+ * 这个优化不是所有前后缀数组问题都可以使用的，得看具体的题目
+ */
+/**
+ * 接雨水问题
+ * 给定一个非负整数数组，代表一个容器，数组中每个位置的数字代表该容器在该位置的高度
+ * 请问，用该容器最多能装多少水
+ * 形象地，将数组画成直方图，每个元素对应直方图中的一个长方形，元素的大小为该长方形的高度
+ * 用该直方图能装下多少面积的水
+ */
+// 借用辅助数组的方法（前缀和后缀）
+int maxRain(vector<int>& container) {
+    int n = container.size();
+    vector<int> left(n, -1);
+    vector<int> right(n, -1);
+    int left_max = container[0], right_max = container[n - 1];
+    for (int i = 1; i < n; i++) {
+        left[i] = left_max;
+        left_max = max(left_max, container[i]);
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        right[i] = right_max;
+        right_max = max(right_max, container[i]);
+    }
+
+    int res = 0;
+    for (int i = 1; i < n - 1; i++) {
+        res += container[i] < min(left[i], right[i]) ? min(left[i], right[i]) - container[i] : 0;
+    }
+
+    return res;
+}
+
+// 额外空间复杂度为O(1)的方法（双指针）
+int maxRain2(vector<int>& container) {
+    int n = container.size(), left_max = container[0], right_max = container[n - 1];
+    int left = 1, right = n - 2, res = 0;
+
+    while(left <= right) {
+        // 如果左指针左边的最大值大于右指针右边的最大值，那么结算右指针所对应区域所能接的雨水
+        // 因为，此时，限制该位置所能接的雨水的数量的瓶颈为右边最大值，此时，无论左边最大值在遍历过程中是否会发生变化，都不会影响该位置的接水量
+        // 此时右指针右边遍历区域的最大值已经确定了，所以该点的右边的最值不可能发生变化，且满足if条件的情况下可以直接结算
+        // 试想，如果左指针的左边的最大值在后续过程中变大，但是仍然满足left_max > right_max，右边仍然是瓶颈
+        // 如果左指针左边的最大值在后续过程中变小，这种情况是不可能出现的，因为最大值只可能变大，不可能变小
+        if (left_max > right_max) {
+            res += container[right] < right_max ? right_max - container[right] : 0;
+            right_max = max(right_max, container[right]);
+            right--;
+        } else {
+            res += container[left] < left_max ? left_max - container[left] : 0;
+            left_max = max(left_max, container[left]);
+            left++;
+        }
+    }
+    return res;
+}
+
+/**
+ * 给定一个数组，该数组可以分为左半部分和右半部分
+ * 请返回左半部分最大值和右半部分最大值相减的 最大值
+ */
+// 前缀后缀数组版本
+int maxLeftPartMinusRightPart(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> left(n, nums[0]);
+    vector<int> right(n, nums[n - 1]);
+    for (int i = 1; i < n; i++) {
+        left[i] = max(nums[i], left[i - 1]);
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        right[i] = max(nums[i], right[i + 1]);
+    }
+
+    int res = 0;
+    for (int i = 0; i < n - 1; i++) {
+        res = max(res, abs(left[i] - right[i + 1]));
+    }
+
+    return res;
+}
+
+// 空间复杂度为O(1)版本
+int maxLeftPartMinusRightPart2(vector<int>& nums) {
+    // 最大值一定会出现在左边部分或者右边部分，所以，最终的答案一定会是最大值减去某个值
+    int max_num = *max_element(nums.begin(), nums.end());
+    // 由于某一部分的最大值都是递增的，所以某一部分的最大值中的最小值只能是其初始值，对于左边来说是第一个元素，右边来说是最后一个元素
+    // 最终结果是返回左边减右边的最大值，所以可以转换成最大值减去左右两边最大值中的最小值，所以就是取min(第一个元素, 最后一个元素)
+    return max_num - min(nums[0], nums[nums.size() - 1]);
+}
+
+
+/**
+ * 判断两个字符串是否互为旋转词
+ * 对于一个字符串，将该字符串的前面任意部分挪到后面，所形成的字符串叫原字符串的旋转词
+ * 现在给定两个字符串str1和str2，请判断两个字符串是否互为旋转词
+ * 假设给定的字符串只包含26个英文小写字母
+ * 注：有些字符串问题都涉及到将字符串拼接再求解，这道题也是这样的方法
+ */
+// 左程云在这里用的是KMP算法
+// 我觉得KMP还是有点繁琐，用滚动哈希来实现
+bool isRotationString(string& str1, string& str2) {
+    if (str1.size() != str2.size()) return false;
+    // 解题的关键在于将其中一个字符串拼接上自己，这样任何的旋转词都会在拼接后的字符串上出现
+    str1 = str1 + str1;
+    // 记录字符串各个前缀子串的哈希值
+    vector<int> hash(str1.size() + 1, 0);
+    int target_hash = str2[0] - 'a', n = str2.size();
+    // d为进制数
+    int d = 131, mod = 1073741824;
+    // 计算被匹配的字符串前缀子串的哈希值
+    for (int i = 0; i < str1.size(); i++) {
+        hash[i + 1] = (hash[i] * d + str1[i] - 'a') % mod;
+    }
+    // 计算目标字符串哈希值
+    for (int i = 1; i < n; i++) {
+        target_hash = (target_hash * d + str2[i] - 'a') % mod;
+    }
+    // 进行字符串匹配
+    for (int i = 0; i < str1.size() - n; i++) {
+        int sub_hash = ((hash[i + n] - hash[i] * int(pow(d, n))) % mod + mod) % mod;  // 被匹配字符串中某一段长度为n的子串哈希值
+        if (sub_hash == target_hash) return true;
+    }
+
+    return false;
+}
+
+
+/**
+ * 咖啡杯问题
+ * 给定一个数组，表示一组咖啡机，数组中的元素代表当前标号的咖啡机制作一杯咖啡的时间
+ * 每一台咖啡机一次只能制作一杯咖啡
+ * 给定一个数N，代表需要喝咖啡的人的数量，一个人只喝一杯咖啡
+ * 一个人喝咖啡的时间为0
+ * 给定一个数a，表示洗咖啡杯的机器，一杯一杯洗，只有当上一杯洗完才能洗下一杯
+ * 给定一个数b，表示咖啡杯不洗也能自动挥发变干净的时间
+ * 问，从一个人开始制作咖啡开始，到最后所有人的咖啡杯都干净所花费的最短时间
+ */
+struct cmp {
+    bool operator() (pair<int, int>& a, pair<int, int>& b) {
+        return a.first + a.second > b.first + b.second;
+    }
+};
+int CoffeeCupProblem(vector<int>& nums, int N, int a, int b) {
+    // 首先，先不关心如何洗咖啡杯，先关心如何使用咖啡机能够在最短的时间内让所有人都喝到咖啡
+    // 显然，这是一个贪心问题，即每次贪结束时间更早的咖啡机，可以建立一个小根堆来选择咖啡机
+    // 小根堆中的元素用pair表示，pair中第一个数表示当前咖啡机在何时能被使用，第二个数表示当前咖啡机制作完一杯咖啡的时间，则某一台咖啡机的结束时间为两个数之和
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> heap;
+    for (int & num : nums) {
+        heap.emplace(0, num);
+    }
+    vector<int> person(N);
+    for (int i = 0; i < N; i++) {
+        auto [start, spend] = heap.top();  // 拿到当前结束时间最早的咖啡机
+        heap.pop();
+        person[i] = start + spend;  // 第i个人何时能获得咖啡，由于喝咖啡时间为0，也表示何时喝完咖啡
+        heap.emplace(start + spend, spend);
+    }
+
+    // 接下来关心何时咖啡杯能够完全干净
+    // 每个人只有两个选择，即用机器洗和自然挥发
+    // 这可以使用从左往右的模型，依次选择并计算结束时间，最终选择结束时间更快的（为了方便理解，这里用缓存写，不用dp写）
+    int max_WashStart = person[0] + a;  // 用于计算最大可能的WashStart
+    for (int i = 1; i < N; i++) {
+        max_WashStart = max(max_WashStart, person[i]) + a;
+    }
+    vector<vector<int>> dp(N, vector<int>(max_WashStart, -1));  // 缓存
+    // 该函数表示，index及之后的人杯子干净的最少时间
+    // index表示人的编号，WashStart表示洗杯子机器最早什么时候可以使用
+    function<int(int, int)> process = [&](int index, int WashStart) {
+        if (dp[index][WashStart] != -1) return dp[index][WashStart];
+        if (index == N - 1) {  // 当来到最后一个人做选择时，选择风干和机器两者中结束时间更小的那一个
+            dp[index][WashStart] = min(person[index] + b, WashStart + a);
+        } else {
+            // 第一种情况，选择使用机器
+            int p1 = max(WashStart, person[index]) + a;  // 当前index人洗完杯子的时间，为什么有max，因为虽然WashStart可以使用，但是此时该人可能还没有喝完咖啡
+            int after1 = process(index + 1, p1);  // 之后的人杯子干净的最短时间
+            int min1 = max(p1, after1);  // 所有杯子干净的时间
+            // 第二种情况选择自然挥发
+            int p2 = person[index] + b;
+            int after2 = process(index + 1, WashStart);
+            int min2 = max(p2, after2);
+            dp[index][WashStart] = min(min1, min2);
+        }
+
+        return dp[index][WashStart];  // 选择花费时间最短的方法
+    };
+
+    return process(0, 0);  // 第0个人及之后杯子干净的最少时间，洗杯子机器在0时可用
+}
+
+
 int main() {
-    vector<vector<int>> m = {{1, 2, 3},
-                             {4, 5, 6},
-                             {7, 8, 9}};
-    cout << Optimize_dp(m) <<endl;
+//    vector<vector<int>> m = {{1, 2, 3},
+//                             {4, 5, 6},
+//                             {7, 8, 9}};
+//    cout << Optimize_dp(m) <<endl;
+//    vector<int> nums = {2,6,7,2,4,7,3};
+//    cout << maxLeftPartMinusRightPart(nums) << endl;
+//    cout << maxLeftPartMinusRightPart2(nums) << endl;
+//    string str1 = "abcd", str2 = "cdcb";
+//    cout << isRotationString(str1, str2);
+    vector<int> nums = {2, 4, 6, 3, 5};
+    cout << CoffeeCupProblem(nums, 10, 3, 5);
     return 0;
 }
